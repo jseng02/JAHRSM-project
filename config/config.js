27741246f16dfe2faff3f9b1c47693b518368540
jseng02 <jseng@my.centennialcollege.ca>
2023-12-06 16:23:15 -0500

    first commit

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST || "mongodb+srv://Blessing:qzzJjISAcwfPklIo@cluster0.txlxgvp.mongodb.net/Skeleton?retryWrites=true&w=majority"||
+    process.env.MONGO_HOST || "mongodb+srv://jseng2:R8uvF7qt8OCTAhmu@cluster0.n2fsuql.mongodb.net/?retryWrites=true&w=majority"||
 process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
@@ -11,3 +11,6 @@ process.env.MONGO_HOST ||
 }
 
 export default config
+
+//"mongodb+srv://jseng2:R8uvF7qt8OCTAhmu@cluster0.n2fsuql.mongodb.net/?retryWrites=true&w=majority"
+//"mongodb+srv://Blessing:qzzJjISAcwfPklIo@cluster0.txlxgvp.mongodb.net/Skeleton?retryWrites=true&w=majority"
