7cd5501610364301ce33910828b769eae1ea2b95
StellaKw <144281244+StellaKw@users.noreply.github.com>
2023-12-06 23:08:18 -0500

    Update theme.jsx
    change in onts and color

--- client/theme.jsx
+++ client/theme.jsx
@@ -1,25 +1,25 @@
-import { createTheme } from '@material-ui/core/styles' 
-import { pink } from '@material-ui/core/colors'
-const theme = createTheme({ 
-typography: {
-useNextVariants: true, 
-},
-palette: {
-primary: {
-light: '#5c67a3', 
-main: '#3f4771', 
-dark: '#2e355b', 
-contrastText: '#fff',
-},
-secondary: {
-light: '#ff79b0', 
-main: '#ff4081', 
-dark: '#c60055', 
-contrastText: '#000',
-},
-openTitle: '#3f4771', 
-protectedTitle: pink['400'], 
-type: 'light'
-} 
-})
-export default theme
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red, blue, green, purple, yellow, teal, pink } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: {
+      light: teal[200],
+      main: teal[500],
+      dark: teal[800],
+    },
+    secondary: {
+      light: pink[200],
+      main: pink[500],
+      dark: pink[800],
+    },
+    openTitle: 'green',
+    protectedTitle: pink['600'],
+    type: 'light',
+  },
+});
+
+export default theme;
