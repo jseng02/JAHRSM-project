6dac4ec0118716d4233c82724ffccc4d05c0c23b
StellaKw <stellakwatepauleleslie@gmail.com>
2023-12-08 22:28:35 -0500

    Updated

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -6,13 +6,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';  
+import Toolbar from '@material-ui/core/Toolbar';  
 import unicornbikeImg from './../assets/images/unicornbikeImg.jpg';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   card: {
-    maxWidth: 600,
+    maxWidth: 700,
     margin: 'auto',
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(3),
     textAlign: 'center',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
   },
@@ -23,7 +25,17 @@ const useStyles = makeStyles(theme => ({
     fontSize: '2rem',
   },
   media: {
-    minHeight: 300,
+    minHeight: 450,
+    position: 'relative',
+    '&:after': {
+      content: "''",
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      top: '100%',
+      background: 'linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.5) 100%)',
+    },
   },
   content: {
     padding: theme.spacing(2),
@@ -40,6 +52,11 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: theme.palette.secondary.dark,
     },
   },
+  copyright: {
+    marginTop: theme.spacing(2),
+    fontSize: '0.8rem',
+    color: '#555',
+  },
 }));
 
 export default function Home() {
@@ -47,24 +64,33 @@ export default function Home() {
   const history = useHistory();
 
   const handleExploreClick = () => {
-    // Redirect to the desired route
     history.push('/explore');
   };
 
   return (
-    <Card className={classes.card}>
-      <Typography variant="h4" className={classes.title}>
-        Welcome to JARHSM
-      </Typography>
-      <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle" />
-      <CardContent className={classes.content}>
-        <Typography variant="body1" className={classes.welcomeText}>
-          Experience the beauty of our E-Commerce Website named JARHSM. Explore and discover all that we offer.
+    <div>
+      <Card className={classes.card}>
+        <Typography variant="h4" className={classes.title}>
+          Welcome to JARHSM
         </Typography>
-        <Button variant="contained" className={classes.exploreButton} onClick={handleExploreClick}>
-          Explore Now
-        </Button>
-      </CardContent>
-    </Card>
+        <CardMedia className={classes.media} image={unicornbikeImg} title="Unicorn Bicycle" />
+        <CardContent className={classes.content}>
+          <Typography variant="body1" className={classes.welcomeText}>
+            Experience the beauty of our E-Commerce Website named JARHSM. Explore and discover all that we offer.
+          </Typography>
+          <Button variant="contained" className={classes.exploreButton} onClick={handleExploreClick}>
+            Explore Now
+          </Button>
+        </CardContent>
+      </Card>
+      <AppBar position="static" style={{ background: '#f2f2f2', color: '#2E3B4E' }}>
+        <Toolbar>
+          <Typography variant="body2" className={classes.copyright}>
+            &copy; 2023 JARHSM E-COMMERCE. All rights reserved.
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
+
--- client/core/Menu.jsx
+++ client/core/Menu.jsx
@@ -22,57 +22,60 @@ const isPartActive = (history, path) => {
 };
 
 const Menu = withRouter(({ history }) => (
-  <AppBar position="static" style={{ background: '#2E3B4E' }}>
-    <Toolbar>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Link to="/">
-          <IconButton aria-label="Home" style={isActive(history, '/')}>
-            <HomeIcon />
-          </IconButton>
-        </Link>
-        <Link to="/shops/all">
-          <Button style={isActive(history, '/shops/all')}>Shops</Button>
-        </Link>
-        <Link to="/cart">
-          <Button style={isActive(history, '/cart')}>
-            My Cart
-            <Badge color="secondary" invisible={false} badgeContent={cart.itemTotal()} style={{ marginLeft: '1px' }}>
-              <CartIcon />
-            </Badge>
-          </Button>
-        </Link>
-      </div>
-      <div style={{ marginLeft: 'auto' }}>
-        <span style={{ float: 'right' }}>
-          {!auth.isAuthenticated() && (
-            <span>
-              <Link to="/signup">
-                <Button style={isActive(history, '/signup')}>Sign Up</Button>
-              </Link>
-              <Link to="/signin">
-                <Button style={isActive(history, '/signin')}>Sign In</Button>
-              </Link>
-            </span>
-          )}
-          {auth.isAuthenticated() && (
-            <span>
-              {auth.isAuthenticated().user.seller && (
-                <Link to="/seller/shops">
-                  <Button style={isPartActive(history, '/seller/')}>View My Shops</Button>
+  <div>
+    <AppBar position="static" style={{ background: '#2E3B4E' }}>
+      <Toolbar>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Link to="/">
+            <IconButton aria-label="Home" style={isActive(history, '/')}>
+              <HomeIcon />
+            </IconButton>
+          </Link>
+          <Link to="/shops/all">
+            <Button style={isActive(history, '/shops/all')}>Shops</Button>
+          </Link>
+          <Link to="/cart">
+            <Button style={isActive(history, '/cart')}>
+              My Cart
+              <Badge color="secondary" invisible={false} badgeContent={cart.itemTotal()} style={{ marginLeft: '1px' }}>
+                <CartIcon />
+              </Badge>
+            </Button>
+          </Link>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', marginLeft: 'auto', alignItems: 'flex-end' }}>
+          <span>
+            {!auth.isAuthenticated() && (
+              <span>
+                <Link to="/signup">
+                  <Button style={isActive(history, '/signup')}>Sign Up</Button>
                 </Link>
-              )}
-              <Link to={`/user/${auth.isAuthenticated().user._id}`}>
-                <Button style={isActive(history, `/user/${auth.isAuthenticated().user._id}`)}>View Profile</Button>
-              </Link>
-              <Button color="inherit" onClick={() => auth.clearJWT(() => history.push('/'))}>
-                Log Out
-              </Button>
-            </span>
-          )}
-        </span>
-      </div>
-    </Toolbar>
-  </AppBar>
+                <Link to="/signin">
+                  <Button style={isActive(history, '/signin')}>Sign In</Button>
+                </Link>
+              </span>
+            )}
+            {auth.isAuthenticated() && (
+              <span>
+                {auth.isAuthenticated().user.seller && (
+                  <Link to="/seller/shops">
+                    <Button style={isPartActive(history, '/seller/')}>View My Shops</Button>
+                  </Link>
+                )}
+                <Link to={`/user/${auth.isAuthenticated().user._id}`}>
+                  <Button style={isActive(history, `/user/${auth.isAuthenticated().user._id}`)}>View Profile</Button>
+                </Link>
+                <Button color="inherit" onClick={() => auth.clearJWT(() => history.push('/'))}>
+                  Log Out
+                </Button>
+              </span>
+            )}
+          </span>
+        </div>
+      </Toolbar>
+    </AppBar>
+    
+  </div>
 ));
 
 export default Menu;
